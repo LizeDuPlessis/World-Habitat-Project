160da2bc27e3fd890cad6517fd2f527b5d85574b
Lize <lizeduplessis.dev@gmail.com>
2022-05-06 08:41:21 +0100

    responsive header

--- Components/Header.jsx
+++ Components/Header.jsx
@@ -5,7 +5,10 @@ import Image from "next/image";
 function Header({ text }) {
   return (
     <header className="flex justify-center ml-5 mt-4">
-      <Image src="/logo.png" width={115} height={110} alt="logo" />
+      <div className="logo">
+        <Image src="/logo.png" width={115} height={110} alt="logo" />
+      </div>
+
       <h2 className="mt-5">{text}</h2>
     </header>
   );
